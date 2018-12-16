51cebf7cd985a6e24f0009968a093142f4ebd986
yeonme <yeon@yeon.me>
2018-12-16 18:08:43 +0900

    set animators

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    Animator[] anims = null;
+    Sprite[] sprites = null;
+    GameObject[] go = null;
+	// Use this for initialization
+	void Start () {
+        go = GameObject.FindGameObjectsWithTag("panda");
+        Debug.Log(go.Length);
+
+        anims = FindObjectsOfType<Animator>();
+        Debug.Log(string.Format("Animators: {0}", anims.Length));
+        Debug.Log(string.Format("Avatar: {0}", anims[0].avatar));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
